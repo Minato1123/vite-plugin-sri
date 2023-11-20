390e5ae262b3b6a1b36d42d570b0707a9899cb67
Minato1123 <slamdunk0522@gmail.com>
2023-11-20 18:09:40 +0800

    build: add .d.ts config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,39 +1,52 @@
 import { readFileSync } from 'fs'
 import { defineConfig } from 'rollup'
-import ts from '@rollup/plugin-typescript';
+import ts from '@rollup/plugin-typescript'
+import { dts } from "rollup-plugin-dts"
 
-export default defineConfig({
-  input: 'index.ts',
-  output: [
-    {
-      file: 'lib/index.cjs',
-      format: 'cjs',
-      exports: 'default',
-      preferConst: true,
+export default defineConfig([
+  {
+    input: 'index.ts',
+    output: [
+      {
+        file: 'lib/index.cjs',
+        format: 'cjs',
+        exports: 'default',
+        preferConst: true,
 
-      // https://nodejs.org/api/esm.html#esm_commonjs_namespaces
-      interop: 'default',
+        // https://nodejs.org/api/esm.html#esm_commonjs_namespaces
+        interop: 'default',
 
-      // Extract license header
-      // @ts-ignore
-      banner: () => readFileSync('index.ts', 'utf8').match(/^\/{4,}[^]+?\/{4,}/)[0],
-    },
-    {
-      file: 'lib/index.js',
+        // Extract license header
+        // @ts-ignore
+        banner: () => readFileSync('index.ts', 'utf8').match(/^\/{4,}[^]+?\/{4,}/)[0],
+      },
+      {
+        file: 'lib/index.js',
+        format: 'esm',
+        exports: 'default',
+        preferConst: true,
+
+        // https://nodejs.org/api/esm.html#esm_commonjs_namespaces
+        interop: 'default',
+
+        // Extract license header
+        // @ts-ignore
+        banner: () => readFileSync('index.ts', 'utf8').match(/^\/{4,}[^]+?\/{4,}/)[0],
+      }
+    ],
+    external: () => true,
+    plugins: [
+      ts(),
+    ],
+  },
+  {
+    input: 'index.ts',
+    plugins: [
+      dts()
+    ],
+    output: {
       format: 'esm',
-      exports: 'default',
-      preferConst: true,
-
-      // https://nodejs.org/api/esm.html#esm_commonjs_namespaces
-      interop: 'default',
-
-      // Extract license header
-      // @ts-ignore
-      banner: () => readFileSync('index.ts', 'utf8').match(/^\/{4,}[^]+?\/{4,}/)[0],
-    }
-  ],
-  external: () => true,
-  plugins: [
-    ts()
-  ]
-})
+      file: 'lib/index.d.ts',
+    },
+  }
+])
